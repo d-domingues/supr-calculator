5d4c70c05607d68edc44572c24af51c707bef126
David Domigues <ddomingues@ampere-energy.com>
2021-08-30 17:29:38 +0200

    basic RegExp log validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <input #input type="text" />
+    <button (click)="onTest(input.value)">TEST</button>
+  `,
 })
 export class AppComponent {
-  title = 'supr-calculator';
+  sum: RegExp = /^[+-]?[0-9]+([+-][0-9]+)*$/;
+  trig: RegExp = /^(sin|cos|tan)\([0-9]+\)$/;
+
+  onTest(value: string) {
+    console.table({
+      sum: this.sum.test(value),
+      trig: this.trig.test(value),
+    });
+  }
 }
